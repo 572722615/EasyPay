03cccd98a5ec4787e6a06eba9fa2c188b75f0db1
w5727 <572722615@qq.com>
2016-04-22 14:17:28 +0800

    add recyclerview

--- app/src/main/java/com/pay/chip/easypay/pages/discount/fragment/DiscountFragment.java
+++ app/src/main/java/com/pay/chip/easypay/pages/discount/fragment/DiscountFragment.java
@@ -2,36 +2,99 @@ package com.pay.chip.easypay.pages.discount.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pay.chip.easypay.R;
+import com.pay.chip.easypay.pages.discount.adapter.DiscountItemRecyclerViewAdapter;
+import com.pay.chip.easypay.pages.discount.model.DummyContent;
+import com.pay.chip.easypay.util.LoadMoreRecyclerView;
 
 
 public class DiscountFragment extends Fragment {
 
 
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private int mColumnCount = 1;
+    private DiscountItemRecyclerViewAdapter myItemRecyclerViewAdapter;
+    private LoadMoreRecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private int page = 0;
 
 
-
-
-    public DiscountFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discount, container, false);
+        View view = inflater.inflate(R.layout.fragment_item_list, container, false);
+        recyclerView = (LoadMoreRecyclerView) view.findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+      /*  view.findViewById(R.id.mode_switch_btn).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (1 == mColumnCount) {
+                    mColumnCount = 2;
+                    ((TextView) v).setText(R.string.list_mode_stagger);
+                    myItemRecyclerViewAdapter.switchMode(true);
+                    recyclerView.switchLayoutManager(new StaggeredGridLayoutManager(mColumnCount, StaggeredGridLayoutManager.VERTICAL));
+                } else {
+                    mColumnCount = 1;
+                    ((TextView) v).setText(R.string.list_mode_list);
+                    myItemRecyclerViewAdapter.switchMode(false);
+                    recyclerView.switchLayoutManager(new LinearLayoutManager(getActivity()));
+                }
+            }
+        });*/
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(false);
+                page = 0;
+                myItemRecyclerViewAdapter.setData(DummyContent.generyData(page));
+                recyclerView.setAutoLoadMoreEnable(DummyContent.hasMore(page));
+                myItemRecyclerViewAdapter.notifyDataSetChanged();
+            }
+        });
+        if (1 >= mColumnCount) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        } else {
+            recyclerView.setLayoutManager(new StaggeredGridLayoutManager(mColumnCount, StaggeredGridLayoutManager.VERTICAL));
+        }
+        myItemRecyclerViewAdapter = new DiscountItemRecyclerViewAdapter(DummyContent.generyData(page));
+        recyclerView.setAdapter(myItemRecyclerViewAdapter);
+        recyclerView.setAutoLoadMoreEnable(true);
+        recyclerView.setLoadMoreListener(new LoadMoreRecyclerView.LoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                recyclerView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        myItemRecyclerViewAdapter.addDatas(DummyContent.generyData(++page));
+                        recyclerView.notifyMoreFinish(DummyContent.hasMore(page));
+                    }
+                }, 1000);
+            }
+        });
+        myItemRecyclerViewAdapter.notifyDataSetChanged();
+        return view;
     }
+
 
 
 
