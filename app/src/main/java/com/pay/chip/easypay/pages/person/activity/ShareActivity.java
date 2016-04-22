65a811c70be3a6f2ecb376b0f3994cb12146d2c1
w5727 <572722615@qq.com>
2016-04-22 19:51:56 +0800

    add news

--- app/src/main/java/com/pay/chip/easypay/pages/person/activity/ShareActivity.java
+++ app/src/main/java/com/pay/chip/easypay/pages/person/activity/ShareActivity.java
@@ -2,14 +2,20 @@ package com.pay.chip.easypay.pages.person.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.pay.chip.easypay.R;
 
-public class ShareActivity extends AppCompatActivity {
+public class ShareActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
